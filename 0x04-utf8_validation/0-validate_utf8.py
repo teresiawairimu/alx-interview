61a8cb20f440e444af847f43b3125a34caf0f49d
teresiawairimu <teresiawairimu06@gmail.com>
2024-10-31 17:50:59 +0000

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,28 +12,24 @@ def validUTF8(data):
         True if data set is valid UTF-8 encoding
         or False if it is invalid
     """
-    num_bytes = 0
-
-    mask1 = 1 << 7  
+    number_of_bytes = 0
+    mask1 = 1 << 7
     mask2 = 1 << 6  
-
     for num in data:
         byte = num & 0xFF
-
-        if num_bytes == 0:
+        if number_of_bytes == 0:
             if (byte & mask1) == 0:
                 continue
             elif (byte & (mask1 >> 1)) == mask1:
-                num_bytes = 1
+                number_of_bytes = 1
             elif (byte & (mask1 >> 2)) == (mask1 >> 1):
-                num_bytes = 2
+                number_of_bytes = 2
             elif (byte & (mask1 >> 3)) == (mask1 >> 2):
-                num_bytes = 3
+                number_of_bytes = 3
             else:
                 return False
         else:
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-            num_bytes -= 1
-    return num_bytes == 0
-
+            number_of_bytes -= 1
+    return number_of_bytes == 0
